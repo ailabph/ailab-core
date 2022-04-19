def332dde5deaa735f3aa47e1717319a90b33b8a
adminailab <96946812+adminailab@users.noreply.github.com>
2022-04-20 00:31:46 +0800

    initial version of Render with Test

--- src/Render.php
+++ src/Render.php
@@ -2,13 +2,175 @@
 
 namespace Ailabph\AilabCore;
 
+use Exception;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class Render
 {
-    static public function Page(){
+    public string $template_path = "";
 
+    /**
+     * @throws Exception
+     */
+    public function __construct(array $options = [])
+    {
+        Tools::importValuesFromArrayToObject($options,$this);
     }
 
-    static public function Section(){
 
+    /**
+     * @throws Exception
+     */
+    static public function getRenderOptions(array $options): Render
+    {
+        return new self($options);
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    static public function page(): string{
+        $pageParam = [];
+        $pageParam["header_content"] = self::getHeader();
+        $pageParam["content"] = self::getContent();
+        $pageParam["footer_content"] = self::getFooter();
+        return self::section("_page.twig", $pageParam);
+    }
+
+    /**
+     * @throws Exception
+     */
+    static public function section(string $template_name, array $pageParam = [], array $options = []): string{
+        Assert::isNotEmpty($template_name);
+        $render_content = "";
+        try{
+            $template_path = self::getRenderOptions($options)->template_path;
+            if(empty($template_path)){
+                $template_path = Config::getBaseDirectory() . "/tpl";
+            }
+
+            $loader = new FilesystemLoader($template_path);
+            $twig = new Environment($loader,["cache"=>false]);
+            $render_content = $twig->render($template_name, $pageParam);
+        }catch (Exception $e){
+            Assert::throw("Unable to render $template_name, ".$e->getMessage());
+        }
+        return $render_content;
+    }
+
+    static public function resetAll(){
+        self::$HEADER_DATA = [];
+        self::$CONTENT_STACKS = [];
+        self::$FOOTER_DATA = [];
+    }
+
+
+
+    # HEADER --------------------------------------------------------------------------------------------------
+
+    static private array $HEADER_DATA = [];
+
+    static public function resetHeaderData(){
+        self::$HEADER_DATA = [];
+    }
+
+    /**
+     * @param string $callable_function
+     * @param object|null $from_object
+     * @throws Exception
+     */
+    static public function addHeader(string $callable_function, $from_object = null){
+        Assert::isNotEmpty($callable_function);
+        self::$HEADER_DATA[] = $callable_function;
+        if(is_object($from_object)){
+            if(!is_callable([$from_object,$callable_function])){
+                Assert::throw("method $callable_function is not callable");
+            }
+            self::$HEADER_DATA[] = $from_object;
+        }
+        else{
+            if(!is_callable($callable_function)){
+                Assert::throw("function $callable_function is not callable");
+            }
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    static public function getHeader(): string{
+        if(count(self::$HEADER_DATA) == 0) return "";
+
+        if(!is_string(self::$HEADER_DATA[0]))
+            Assert::throw("first argument of header data must be a callable string");
+
+        if(isset(self::$HEADER_DATA[1]) && !is_object(self::$HEADER_DATA[1]))
+            Assert::throw("second argument for header data must be an object");
+
+        if(isset(self::$HEADER_DATA[1])){
+            return call_user_func([self::$HEADER_DATA[1], self::$HEADER_DATA[0]]);
+        }
+
+        return call_user_func(self::$HEADER_DATA[0]);
+    }
+
+    # FOOTER --------------------------------------------------------------------------------------------------
+
+    static private array $FOOTER_DATA = [];
+
+    static public function resetFooterData(){
+        self::$FOOTER_DATA = [];
+    }
+
+    /**
+     * @throws Exception
+     */
+    static public function addFooter(string $callable_function, $from_object = null){
+        Assert::isNotEmpty($callable_function);
+        self::$FOOTER_DATA[] = $callable_function;
+        if(is_object($from_object)){
+            self::$FOOTER_DATA[] = $from_object;
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    static public function getFooter(): string{
+        if(count(self::$FOOTER_DATA) == 0) return "";
+
+        if(!is_string(self::$FOOTER_DATA[0]))
+            Assert::throw("first argument of header data must be a callable string");
+
+        if(isset(self::$FOOTER_DATA[1]) && !is_object(self::$FOOTER_DATA[1]))
+            Assert::throw("second argument for header data must be an object");
+
+        if(isset(self::$FOOTER_DATA[1])){
+            return call_user_func([self::$FOOTER_DATA[1], self::$FOOTER_DATA[0]]);
+        }
+
+        return call_user_func(self::$FOOTER_DATA[0]);
+    }
+
+
+    # CONTENT STACKS ------------------------------------------------------------------------------------------------
+
+    static private array $CONTENT_STACKS = [];
+
+    static public function resetContentStacks(){
+        self::$CONTENT_STACKS = [];
+    }
+
+    /**
+     * @throws Exception
+     */
+    static public function addContent(string $template_name, array $contentParam = [], array $options =[]){
+        self::$CONTENT_STACKS[] = self::section($template_name, $contentParam, $options);
+    }
+
+    static public function getContent(): string{
+        return implode('', self::$CONTENT_STACKS);
     }
 }
--- tests/RenderTest.php
+++ tests/RenderTest.php
@@ -1,11 +1,181 @@
 <?php
 use Ailabph\AilabCore;
 use PHPUnit\Framework\TestCase;
+use org\bovigo\vfs;
 
 class RenderTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        AilabCore\Render::resetAll();
+    }
+
+    # ---------------------------------------------------------------------------------------------------------
+
     public function testCreateObject(){
         $render = new AilabCore\Render();
         $this->assertIsObject($render,"Render Object");
     }
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderSectionOnDefaultTemplateDirectory(){
+        $page_content = AilabCore\Render::section("_page.twig");
+        self::assertIsString($page_content,"page_content");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderSectionOnCustomDirectory(){
+        $test_dir_name = "test_dir_".AilabCore\Random::getRandomStr(5)."/tpl";
+        vfs\vfsStream::setup($test_dir_name);
+        $test_path = vfs\vfsStream::url($test_dir_name."/");
+        $test_template_file = $test_path."test.twig";
+        file_put_contents($test_template_file,"<h2>{{ test_title }}</h2>");
+
+        $check_content = AilabCore\Render::section("test.twig",["test_title"=>"this is a title"],["template_path"=>$test_path]);
+        self::assertEquals("<h2>this is a title</h2>",$check_content);
+    }
+
+
+    # HEADER ---------------------------------------------------------------------------------------------------------
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderGetHeaderCallableFunction(){
+        AilabCore\Render::resetHeaderData();
+        AilabCore\Render::addHeader("sampleHeaderFunction");
+        $content = AilabCore\Render::getHeader();
+        self::assertIsString($content);
+        self::assertEquals("<h1>Header Content from a function</h1>",$content);
+    }
+
+    public function sampleMethodForHeader(): string{
+        return "<h1>Header Content from a method</h1>";
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderGetHeaderCallableMethod(){
+        AilabCore\Render::resetHeaderData();
+        AilabCore\Render::addHeader("sampleMethodForHeader",$this);
+        $content = AilabCore\Render::getHeader();
+        self::assertEquals("<h1>Header Content from a method</h1>",$content);
+    }
+
+    public function testAddHeaderFunctionNotCallableThrowError(){
+        AilabCore\Render::resetHeaderData();
+        self::expectException(Exception::class);
+        AilabCore\Render::addHeader("randomFunction");
+    }
+
+    public function testAddHeaderMethodNotCallableThrowError(){
+        AilabCore\Render::resetHeaderData();
+        self::expectException(Exception::class);
+        AilabCore\Render::addHeader("randomMethod",$this);
+    }
+
+
+    # FOOTER ---------------------------------------------------------------------------------------------------------
+
+
+    public function sampleMethodForFooter(): string{
+        return "<script>console.log('footer content from a method');</script>";
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderGetFooterCallableFunction(){
+        AilabCore\Render::resetFooterData();
+        AilabCore\Render::addFooter("sampleFooterFunction");
+        $content = AilabCore\Render::getFooter();
+        self::assertIsString($content);
+        self::assertEquals("<script>console.log('footer content from a function');</script>",$content);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderGetFooterCallableMethod(){
+        AilabCore\Render::resetFooterData();
+        AilabCore\Render::addFooter("sampleMethodForFooter",$this);
+        $content = AilabCore\Render::getFooter();
+        self::assertEquals("<script>console.log('footer content from a method');</script>",$content);
+    }
+
+
+    # SECTION STACKS -------------------------------------------------------------------------------------------------
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderAddContent(){
+        AilabCore\Render::addContent("_content.twig",["content"=>"Content123"]);
+        AilabCore\Render::addContent("_content.twig",["content"=>"Content456"]);
+        $content = AilabCore\Render::getContent();
+        self::assertIsString($content,"content");
+        self::assertEquals("Content123Content456",$content);
+    }
+
+
+
+    # PAGE ---------------------------------------------------------------------------------------------------------
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderPageBasicParts(){
+        $page_content = AilabCore\Render::page();
+        self::assertIsString($page_content,"page_content");
+        self::assertStringContainsString("<html",$page_content);
+        self::assertStringContainsString("<head>",$page_content);
+        self::assertStringContainsString("</head>",$page_content);
+        self::assertStringContainsString("<body",$page_content);
+        self::assertStringContainsString("</body>",$page_content);
+        self::assertStringContainsString("</html>",$page_content);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderPageAfterAddingContent(){
+        AilabCore\Render::addContent("_content.twig",["content"=>"345"]);
+        AilabCore\Render::addContent("_content.twig",["content"=>"678"]);
+        $page_content = AilabCore\Render::page();
+        self::assertIsString($page_content,"page_content");
+        self::assertStringContainsString("<body",$page_content);
+        self::assertStringContainsString("345678",$page_content);
+        self::assertStringContainsString("</body>",$page_content);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRenderPageAfterAddingHeadContentFooter(){
+        AilabCore\Render::addHeader("sampleHeaderFunction2");
+        AilabCore\Render::addFooter("sampleFooterFunction");
+        AilabCore\Render::addContent("_content.twig",["content"=>"this within the body is the content"]);
+        $page_content = AilabCore\Render::page();
+        self::assertStringContainsString("<style></style>", $page_content);
+        self::assertStringContainsString("footer content from a function", $page_content);
+        self::assertStringContainsString("this within the body is the content", $page_content);
+    }
+
+}
+
+function sampleHeaderFunction(): string{
+    return "<h1>Header Content from a function</h1>";
+}
+
+function sampleHeaderFunction2(){
+    return "<style></style>";
+}
+
+function sampleFooterFunction(): string{
+    return "<script>console.log('footer content from a function');</script>";
 }
