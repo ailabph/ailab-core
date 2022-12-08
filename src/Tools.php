16668399b8cc81fbdfb1e1e65957450ddfe23aba
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-09 00:15:11 +0800

    update tools module

--- src/Tools.php
+++ src/Tools.php
@@ -28,4 +28,34 @@ class Tools
             $to_object->{$property} = is_null($extracted_value) && isset($value) ? $value : $extracted_value;
         }
     }
+
+    /**
+     * @throws Exception
+     */
+    public static function log(string $message, string $category = "", bool $force_write = false, bool $print_trace = false): void{
+        if(Config::getConfig()->verbose_log || $force_write){
+            Assert::isNotEmpty($message,"log message");
+
+            $log_directory = Config::getBaseDirectory() . "/logs";
+            $file_name = $log_directory . "/log_";
+            if(!empty($category)){
+                $file_name .= $category ."_";
+            }
+            $file_name .= TimeHelper::getCurrentTime()->format("Ymd").".log";
+
+            $formatted_message = TimeHelper::getCurrentTime()->format(TimeHelper::FORMAT_DATE_TIME_AMPM);
+            $formatted_message = "[$formatted_message] ".$message;
+            $formatted_message .= PHP_EOL;
+            file_put_contents($file_name, $formatted_message, FILE_APPEND);
+
+            if($print_trace){
+                try{
+                    throw new Exception($message);
+                }catch (Exception $e){
+                    $trace = $e->getTraceAsString();
+                    self::log(message:$trace,category:$category,force_write: $force_write);
+                }
+            }
+        }
+    }
 }
--- tests/ToolsTest.php
+++ tests/ToolsTest.php
@@ -1,10 +1,66 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
+use Ailabph\AilabCore;
 use Ailabph\AilabCore\Tools;
+use PHPUnit\Framework\TestCase;
+use org\bovigo\vfs;
 
 class ToolsTest extends TestCase
 {
+    #region SETUP MOCK CONFIG
+    static private string $original_DIR = "";
+    static string $RANDOM_DIR_NAME = "";
+    public string $originalScriptDir = "";
+
+    public static function setUpBeforeClass():void
+    {
+        AilabCore\Config::resetCache();
+        AilabCore\Config::resetOverrideEnv();
+        self::$original_DIR = AilabCore\Config::$CURRENT_DIR;
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        AilabCore\Config::$CURRENT_DIR = self::$original_DIR;
+    }
+
+    protected function setUp(): void{
+        vfs\vfsStream::setup($this->getTestDir());
+        vfs\vfsStream::setup($this->getTestDir()."/logs");
+        AilabCore\Config::$OVERRIDE_PATH = vfs\vfsStream::url($this->getTestDir()."/");
+
+        $content = ";<?php die();".PHP_EOL;
+        $content .= ";/*".PHP_EOL;
+        $content .= 'db_host = "db_host" '.PHP_EOL;
+        $content .= 'db_name = "test_db_name" '.PHP_EOL;
+        $content .= 'db_user = "db_user" '.PHP_EOL;
+        $content .= 'db_pass = "db_pass" '.PHP_EOL;
+        $content .= ";*/".PHP_EOL;
+        file_put_contents(AilabCore\Config::$OVERRIDE_PATH."/config.ini.php",$content);
+        $content = ";<?php die();".PHP_EOL;
+        $content .= ";/*".PHP_EOL;
+        $content .= 'test_prop = "test_value"'.PHP_EOL;
+        file_put_contents(AilabCore\Config::$OVERRIDE_PATH."/config_public.ini.php",$content);
+
+        $this->originalScriptDir = AilabCore\Config::$CURRENT_DIR;
+    }
+
+    protected function tearDown(): void
+    {
+        AilabCore\Config::$OVERRIDE_PATH = "";
+        AilabCore\Config::$CURRENT_DIR = self::$original_DIR;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getTestDir(): string{
+        if(!empty(static::$RANDOM_DIR_NAME)) return static::$RANDOM_DIR_NAME;
+        static::$RANDOM_DIR_NAME = AilabCore\Random::getRandomStr(5,AilabCore\Random::OPTION_ALPHA_LOW);
+        return static::$RANDOM_DIR_NAME;
+    }
+    #endregion
+
     /**
      * @throws Exception
      */
