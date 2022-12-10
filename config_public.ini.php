ec7987a8ae5cd68e2fd9bb5e09b360d45ae20fd7
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-10 22:48:49 +0800

    add sample config public

--- config_public.ini.php
+++ config_public.ini.php
@@ -1,1 +1,11 @@
-<?php
+;<?php die();
+;/*
+
+site_name = "AI LAB CORE";
+site_name_short = "AILABCORE";
+site_tagline = "AI";
+site_prefix = "A";
+site_shortcode = "AILAB";
+site_logo_box = "logo-box.png";
+site_logo_wide = "logo-wide.png";
+message = "hello";
