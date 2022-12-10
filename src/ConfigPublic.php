00cd069f002442c9f6c7f8f1f22a9c1e6489db0a
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-10 22:47:32 +0800

    added base clas for config public

---
+++ src/ConfigPublic.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ailabph\AilabCore;
+
+/**
+ * Extend this class to populate what is inside the public config file
+ */
+class ConfigPublic
+{
+    public string $site_name = "";
+    public string $site_name_short = "";
+    public string $site_tagline = "";
+    public string $site_prefix = "";
+    public string $site_shortcode = "";
+    public string $site_lgo_box = "";
+    public string $site_logo_wide = "";
+}
