603727155f7c0368e205596823b1c11cfbcf2057
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-12 22:40:53 +0800

    update public config properties

--- src/ConfigPublic.php
+++ src/ConfigPublic.php
@@ -12,6 +12,13 @@ class ConfigPublic
     public string $site_tagline = "";
     public string $site_prefix = "";
     public string $site_shortcode = "";
-    public string $site_lgo_box = "";
+    public string $site_logo_box = "";
     public string $site_logo_wide = "";
+    public string $theme_url = "";
+    public string $login_bg = "";
+    public string $logo_login = "";
+    public string $logo_box = "";
+    public string $logo_wide = "";
+    public string $logo_bg = "";
+    public string $structure_bg = "";
 }
