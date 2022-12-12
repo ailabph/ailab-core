3a40cd46aa32182f16b163cb2e525de5f25b8472
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-12 22:41:45 +0800

    update tools methods

--- src/Tools.php
+++ src/Tools.php
@@ -30,12 +30,26 @@ class Tools
         if(!self::isLoggedIn()) Assert::throw("unable to retrieve current user, not logged in");
         return $GLOBALS["user"];
     }
+
+    public static function getIpAddress(){
+        if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
+                $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+                return trim($addr[0]);
+            } else {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+        }
+        else {
+            return $_SERVER['REMOTE_ADDR'] ?? "";
+        }
+    }
     #endregion
 
 
     #region CHECKERS
     static public function isLoggedIn(): bool{
-        return isset($GLOBALS["user"]);
+        return isset($GLOBALS["user"]) && $GLOBALS["user"] instanceof DB\userX;
     }
     #endregion
 
@@ -119,6 +133,19 @@ class Tools
 
 
     #region DATA PROCESS
+
+    public static function convertArrayOfStringToString(array $array, string $separator, string $data_wrapper = ""): string{
+        Assert::isNotEmpty($separator,"array separator");
+        $new_data = [];
+        foreach ($array as $item=>$value){
+            if(!empty($data_wrapper)){
+                $value = $data_wrapper . $value . $data_wrapper;
+            }
+            $new_data[] = $value;
+        }
+        return implode($separator,$new_data);
+    }
+
     #endregion END OF DATA PROCESS
 
 
@@ -148,21 +175,81 @@ class Tools
         }
     }
 
-
-
-    public static function getIpAddress(){
-        if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
-            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
-                $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
-                return trim($addr[0]);
-            } else {
-                return $_SERVER['HTTP_X_FORWARDED_FOR'];
-            }
-        }
-        else {
-            return $_SERVER['REMOTE_ADDR'] ?? "";
-        }
+    static public function isInModule(): bool{
+        return str_contains(__DIR__,"/vendor/");
     }
+
+    static public function getDefaultPageParam(): array{
+        $param["MAINTENANCE_MODE"] = Config::getConfig()->maintenance_mode;
+        $param["maintenance_mode"] = Config::getConfig()->maintenance_mode;
+
+        $param["MAINTENANCE_MESSAGE"] = Config::getConfig()->maintenance_mode_message;
+        $param["maintenance_message"] = Config::getConfig()->maintenance_mode_message;
+
+        $param["user"] = Tools::isLoggedIn() ? Tools::getCurrentUser() : null;
+
+        $param["ROOTDIRECTORY"] = Config::getBaseDirectory();
+        $param["base_dir"] = Config::getBaseDirectory();
+
+        $param["module_dir"] = Config::getBaseDirectory(of_core_module: true);
+
+        if(self::isInModule()){
+            $param["core_script_url"] = Config::getConfig()->site_url . "/vendor/ailabph/ailab-core/core_scripts";
+        }
+        else{
+            $param["core_script_url"] = Config::getConfig()->site_url . "/core_scripts";
+        }
+
+        $param["page"] = Render::getPage();
+        $param["page_details"] = Render::getPageDetails();
+        $param["page_description"] = Render::getPageDescription();
+
+        $param["SITE_NAME"] = Config::getPublicConfig()->site_name;
+        $param["site_name"] = Config::getPublicConfig()->site_name;
+
+        $param["SENDERNAME"] = Config::getPublicConfig()->site_shortcode;
+
+        $param["URL"] = Config::getConfig()->site_url;
+        $param["APP_URL"] = config::getConfig()->site_url;
+        $param["url"] = Config::getConfig()->site_url;
+
+        $param["SITE_URL"] = Config::getConfig()->site_front_url;
+        $param["SITE_URL_SHORT"] = Config::getConfig()->site_front_url;
+        $param["site_url"] = Config::getConfig()->site_front_url;
+
+
+        $param["APP_URL_SHORT"] = Config::getConfig()->site_domain;
+        $param["site_domain"] = Config::getConfig()->site_domain;
+
+        $param["THEME_URL"] = Config::getPublicConfig()->theme_url;
+        $param["theme_url"] = Config::getPublicConfig()->theme_url;
+
+        $param["SITE_PREFIX"] = Config::getPublicConfig()->site_prefix;
+        $param["site_prefix"] = Config::getPublicConfig()->site_prefix;
+
+        $param["ENABLE_OTP"] = Config::getConfig()->enable_otp;
+        $param["enable_otp"] = Config::getConfig()->enable_otp;
+
+        $param["LOGIN_BG"] = Config::getPublicConfig()->login_bg;
+        $param["login_bg"] = Config::getPublicConfig()->login_bg;
+
+        $param["LOGIN_LOGO"] = Config::getPublicConfig()->logo_login;
+        $param["logo_login"] = Config::getPublicConfig()->logo_login;
+
+        $param["LOGO"] = Config::getPublicConfig()->logo_wide;
+        $param["logo_box"] = Config::getPublicConfig()->logo_box;
+        $param["logo_wide"] = Config::getPublicConfig()->logo_wide;
+
+        $param["STRUCTURE_BG"] = Config::getPublicConfig()->structure_bg;
+        $param["structure_bg"] = Config::getPublicConfig()->structure_bg;
+
+        $param["LOGIN_BG"] = Config::getPublicConfig()->login_bg;
+        $param["login_bg"] = Config::getPublicConfig()->login_bg;
+
+        return $param;
+    }
+
+
 
 
 }
