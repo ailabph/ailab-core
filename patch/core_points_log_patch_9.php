ace62e80ff6ae1652da66e25eadb7645c085aa6b
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-13 21:36:29 +0800

    patch add variant_tag in points_log

---
+++ patch/core_points_log_patch_9.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\DBClassGenerator\DB;
+use Ailabph\AilabCore;
+
+$statement = AilabCore\Connection::getConnection()
+    ->prepare("ALTER TABLE `points_log` ADD `variant_tag` VARCHAR(255) NULL;");
+$statement->execute();
