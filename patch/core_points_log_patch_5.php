9d3fa5a7deabf4c98f30b39c9d6f47acd53e62b6
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-13 21:35:39 +0800

    patch data_value remarks

---
+++ patch/core_points_log_patch_5.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\DBClassGenerator\DB;
+use Ailabph\AilabCore;
+
+$statement = AilabCore\Connection::getConnection()
+    ->prepare("ALTER TABLE `points_log` ADD `data_value_remarks` VARCHAR(255) NULL;");
+$statement->execute();
