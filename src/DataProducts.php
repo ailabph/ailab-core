1f7b0ec01de3f6f95dd60d10cab8db7a282e74ee
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-16 16:59:34 +0800

    add pure flag, not use X class

--- src/DataProducts.php
+++ src/DataProducts.php
@@ -6,31 +6,31 @@ use App\DBClassGenerator\DB;
 
 class DataProducts
 {
-    public static function get(DB\products|string|int $products): DB\productsX
+    public static function get(DB\products|string|int $products, bool $pure = false): DB\productsX|DB\products
     {
         if(!class_exists(DB\productsX::class)){
             Assert::throw("must implement productsX");
         }
-        $to_return = new DB\productsX();
+        $product_class_to_use = $pure ? DB\products::class : DB\productsX::class;
+        $to_return = new $product_class_to_use();
         $get_method = "";
 
-        if($products instanceof DB\productsX){
-            $get_method = ", via passed productsX";
+        if($products instanceof $product_class_to_use){
             $to_return = $products;
         }
-        else if($products instanceof DB\package_header){
+        else if($products instanceof DB\products && !$pure){
             $get_method = ", via conversion to productsX id:$products->id";
             $to_return = new DB\productsX(["id"=>$products->id]);
         }
 
         if($to_return->isNew() && is_numeric($products)){
             $get_method = ", via product id:$products";
-            $to_return = new DB\productsX(["id"=>$products]);
+            $to_return = new $product_class_to_use(["id"=>$products]);
         }
 
         if($to_return->isNew() && is_string($products)){
             $get_method = ", via product_tag:$products";
-            $to_return = new DB\productsX(["product_tag"=>$products]);
+            $to_return = new $product_class_to_use(["product_tag"=>$products]);
         }
 
         if($to_return->isNew()){
