085efd1d4a36502545d5df84f620583f15b8f5a3
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-16 15:29:25 +0800

    fix bug not able to log if called via hook call

--- src/Logger.php
+++ src/Logger.php
@@ -15,17 +15,21 @@ class Logger
         if(empty($call_stack)) Assert::throw("unable to log, call stack empty");
         $call_stack = $call_stack[count($call_stack)-1];
 
+        if(!isset($call_stack["file"])){
+            $source = str_replace( TableClass::BASE_NAME_SPACE,"", $call_stack["class"]);
+        }
+        else{
+            $file_source = str_replace(Config::getBaseDirectory(),"",$call_stack["file"]);
+            $file_source_parts = explode("/",$file_source);
+            $source = $file_source_parts[count($file_source_parts)-1];
+        }
+        $method = $call_stack["function"];
+
         if(!is_string($msg)){
             $msg = PHP_EOL.print_r($msg,true);
         }
 
-        $file_source = str_replace(Config::getBaseDirectory(),"",$call_stack["file"]);
-        $method = $call_stack["function"];
-
-        $file_source_parts = explode("/",$file_source);
-        $file_name = $file_source_parts[count($file_source_parts)-1];
-
-        $build_log = "[$file_name:$line|$method()] ".$msg;
+        $build_log = "[$source:$line|$method()] ".$msg;
 
         Tools::log(message:$build_log,category: $category,force_write: $always_write);
     }
