36dabfbe391f2c43656b8cc853f738fd17fc6d06
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-17 23:19:52 +0800

    refactor

--- src/DataPackage.php
+++ src/DataPackage.php
@@ -5,37 +5,24 @@ use App\DBClassGenerator\DB;
 
 class DataPackage
 {
-    public static function get(DB\package_header|string|int $package): DB\package_headerX
+    private static bool $initiated = false;
+    private static function init(){
+        if(self::$initiated) return;
+        $package_header = Tools::appClassExist("package_header");
+        $package_headerX = Tools::appClassExist("package_headerX");
+        self::$initiated = true;
+    }
+
+    public static function get(DB\package_header|string|int $package, bool $baseOnly = false): DB\package_headerX|DB\package_header
     {
-        if(!class_exists(DB\package_headerX::class)){
-            Assert::throw("must implement package_headerX");
-        }
-        $to_return = new DB\package_headerX();
-        $get_method = "";
-
-        if($package instanceof DB\package_headerX){
-            $get_method = ", via passed package_headerX";
-            $to_return = $package;
-        }
-        else if($package instanceof DB\package_header){
-            $get_method = ", via conversion to package_headerX id:$package->id";
-            $to_return = new DB\package_headerX(["id"=>$package->id]);
-        }
-
-        if($to_return->isNew() && is_numeric($package)){
-            $get_method = ", via package_id:$package";
-            $to_return = new DB\package_headerX(["id"=>$package]);
-        }
-
-        if($to_return->isNew() && is_string($package)){
-            $get_method = ", via package_tag:$package";
-            $to_return = new DB\package_headerX(["package_tag"=>$package]);
-        }
-
-        if($to_return->isNew()){
-            Assert::throw("unable to retrieve package".$get_method);
-        }
-
-        return $to_return;
+        self::init();
+        return DataGeneric::get(
+            base_class: "order_header",
+            extended_class: "order_headerX",
+            dataObj: $package,
+            priKey: "id",
+            uniKey:"package_tag",
+            baseOnly:$baseOnly
+        );
     }
 }
