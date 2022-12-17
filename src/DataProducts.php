da6b39d47c4655e38e2d4de4af37f6443969cbf7
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-17 23:21:44 +0800

    refactor

--- src/DataProducts.php
+++ src/DataProducts.php
@@ -6,37 +6,23 @@ use App\DBClassGenerator\DB;
 
 class DataProducts
 {
-    public static function get(DB\products|string|int $products, bool $pure = false): DB\productsX|DB\products
+    private static bool $initiated = false;
+    private static function init(){
+        if(self::$initiated) return;
+        $products = Tools::appClassExist("products");
+        $productsX = Tools::appClassExist("productsX");
+        self::$initiated = true;
+    }
+
+    public static function get(DB\products|string|int $products, bool $baseOnly = false): DB\productsX|DB\products
     {
-        if(!class_exists(DB\productsX::class)){
-            Assert::throw("must implement productsX");
-        }
-        $product_class_to_use = $pure ? DB\products::class : DB\productsX::class;
-        $to_return = new $product_class_to_use();
-        $get_method = "";
-
-        if($products instanceof $product_class_to_use){
-            $to_return = $products;
-        }
-        else if($products instanceof DB\products && !$pure){
-            $get_method = ", via conversion to productsX id:$products->id";
-            $to_return = new DB\productsX(["id"=>$products->id]);
-        }
-
-        if($to_return->isNew() && is_numeric($products)){
-            $get_method = ", via product id:$products";
-            $to_return = new $product_class_to_use(["id"=>$products]);
-        }
-
-        if($to_return->isNew() && is_string($products)){
-            $get_method = ", via product_tag:$products";
-            $to_return = new $product_class_to_use(["product_tag"=>$products]);
-        }
-
-        if($to_return->isNew()){
-            Assert::throw("unable to retrieve product".$get_method);
-        }
-
-        return $to_return;
+        return DataGeneric::get(
+            base_class: "products",
+            extended_class: "productsX",
+            dataObj: $products,
+            priKey: "id",
+            uniKey:"product_tag",
+            baseOnly:$baseOnly
+        );
     }
 }
