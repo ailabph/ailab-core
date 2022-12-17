0c212a8c1df00704a5025c4b522e442c6fcc9f2e
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-17 23:18:29 +0800

    refactor. implement generation of entry and product codes

--- src/DataCodes.php
+++ src/DataCodes.php
@@ -2,34 +2,330 @@
 
 namespace Ailabph\AilabCore;
 use App\DBClassGenerator\DB;
-class DataCodes
+use PhpParser\Node\Expr\AssignOp\ShiftLeft;
+
+class DataCodes implements Loggable
 {
-    public static function getRecord(DB\codes|string|int $codes): DB\codesX{
-        $to_return = new DB\codesX();
-        $get_method = "";
+    private static bool $initiated = false;
+    private static function init(){
+        if(self::$initiated) return;
+        $order_header = Tools::appClassExist("order_header");
+        $payment = Tools::appClassExist("payment");
+        $package_variant = Tools::appClassExist("package_variant");
+        $codesX = Tools::appClassExist("codesX");
+        Tools::checkPropertiesExistInClass($codesX,[
 
-        if($codes instanceof DB\codesX){
-            $get_method = ", via passed codesX object";
-            $to_return = $codes;
-        }
-        else if($codes instanceof DB\codes){
-            $get_method = ", via conversion to codesX object, id:$codes->id";
-            $to_return = self::getRecord($codes->id);
-        }
+        ]);
 
-        if($to_return->isNew() && is_numeric($codes)){
-            $get_method = ", via codes id:$codes";
-            $to_return = new DB\codesX(["id"=>$codes]);
-        }
-
-        if($to_return->isNew() && is_string($codes)){
-            $get_method = ", via code:$codes";
-            $to_return = new DB\codesX(["code"=>$codes]);
-        }
-
-        if($to_return->isNew()){
-            Assert::throw("codes record not retrieved from database".$get_method);
-        }
-        return $to_return;
+        self::$initiated = true;
     }
+    public static function get(DB\codes|string|int $codes,bool $baseOnly = false, bool $throw = true): DB\codesX|DB\codes|false{
+        self::init();
+        return DataGeneric::get(
+            base_class: "codes",
+            extended_class: "codesX",
+            dataObj: $codes,
+            priKey: "id",
+            uniKey:"code",
+            baseOnly:$baseOnly,
+            throw: $throw
+        );
+    }
+
+    #region ENTRY
+    public static function createNewEntryCodes(
+        int|string|DB\package_variant $variant,
+        int $qty,
+        bool $is_auto_generated = false,
+        bool $is_bundle = false,
+        ...$args
+    ): DB\codesList
+    {
+        Assert::inTransaction();
+        $variant = DataPackageVariant::get($variant);
+        Assert::isGreaterThan($qty,0,"qty");
+
+        /** @var DB\payment $payment */
+        $payment = DataGeneric::getDataObjectsFromArray($args,"payment");
+        /** @var DB\order_header $order_header */
+        $order_header = DataGeneric::getDataObjectsFromArray($args,"order_header");
+        /** @var DB\order_detail $order_detail */
+        $order_detail = DataGeneric::getDataObjectsFromArray($args,"order_detail");
+        /** @var DB\user $owner */
+        $owner = DataGeneric::getDataObjectsFromArray($args,"user");
+
+        self::hasGeneratedCodeFromPaymentOrCenterRelease($payment,$order_header);
+
+        if(!$owner){
+            $owner = Session::getCurrentUser(throw:true);
+        }
+
+        $new_codes = new DB\codesList(" WHERE 0 ",[]);
+        for($x=0;$x<$qty;$x++){
+            $code = self::createNewEntryCode($variant,$owner,$payment,$order_header,$order_detail);
+            self::checkCodeIntegrity($code);
+            $code->save();
+            $user_from = $order_header ? DataUser::get($order_header->user_id) : null;
+            DataCodeOwnershipLog::addLog(DB\code_ownership_logX::$ACTIONS["purchase"],$code,$user_from,$owner,$code->time_purchased > 0 ? $code->time_purchased : $code->time_generated);
+            $new_codes->list[] = $code;
+        }
+        //endregion
+        return $new_codes;
+    }
+
+    public static function createNewEntryCode(
+        DB\package_variant $variant,
+        ...$args,
+    ):DB\codesX{
+        $owner = null;
+        $payment = null;
+        $order_header = null;
+        $order_detail = null;
+        $approved_by_user = null;
+        foreach ($args as $arg){
+            if($arg instanceof DB\user){
+                $owner = $arg;
+            }
+            else if($arg instanceof DB\payment){
+                $payment = $arg;
+            }
+            else if($arg instanceof DB\order_header){
+                $order_header = $arg;
+            }
+            else if($arg instanceof DB\order_detail){
+                $order_detail = $arg;
+            }
+        }
+
+        $new_code = new DB\codesX();
+        if($payment){
+            $owner = DataUser::get($payment->created_by);
+            $approved_by_user = DataUser::get($payment->approved_by);
+
+            $new_code->payment_ref = $payment->payment_referrence;
+            $new_code->time_purchased = $payment->time_approved;
+            if($payment->ne_order > 0){
+                $new_code->ne_order_id = $payment->ne_order;
+            }
+        }
+        else if($order_header){
+            if(is_null($order_detail) && !($order_detail instanceof DB\order_detail)){
+                Assert::throw(error_message:"order_detail is required",critical_error: true);
+            }
+            $approved_by_user = DataUser::get($order_header->user_id);
+            $owner = $approved_by_user;
+            if($order_header->order_for > 0){
+                $owner = DataUser::get($order_header->order_for);
+            }
+            $new_code->order_id = $order_header->id;
+            $new_code->order_detail = $order_detail->id;
+        }
+        else if($owner){
+            $approved_by_user = $owner;
+        }
+        else{
+            $owner = Session::getCurrentUser(true);
+            $approved_by_user = $owner;
+        }
+
+        $new_code->code_type = "e";
+        $new_code->package_id = $variant->package_id;
+        $new_code->variant_id = $variant->id;
+        $new_code->variant_tag = $variant->package_tag;
+        $new_code->code = self::generateAccountNumber();
+        $new_code->pin = Random::getRandomInt(1000,9999);
+        $new_code->time_generated = TimeHelper::getCurrentTime()->getTimestamp();
+        $new_code->status = "o";
+        $new_code->purchased_by = $owner->id;
+        $new_code->owned_by = $owner->id;
+        $new_code->approved_by = $approved_by_user->id;
+        $new_code->save();
+        return $new_code;
+    }
+    #endregion END ENTRY
+
+
+    #region PRODUCTS
+    public static function createNewProductCodes(
+        int|string|DB\products $product,
+        int $qty,
+        ...$args
+    ): DB\codesList
+    {
+        Assert::inTransaction();
+        $product = DataProducts::get($product);
+        Assert::isGreaterThan($qty,0,"qty");
+
+        /** @var DB\payment $payment */
+        $payment = DataGeneric::getDataObjectsFromArray($args,"payment");
+        /** @var DB\payment_details $payment_details */
+        $payment_details = DataGeneric::getDataObjectsFromArray($args,"payment_details");
+        /** @var DB\order_header $order_header */
+        $order_header = DataGeneric::getDataObjectsFromArray($args,"order_header");
+        /** @var DB\order_detail $order_detail */
+        $order_detail = DataGeneric::getDataObjectsFromArray($args,"order_detail");
+        /** @var DB\user $owner */
+        $owner = DataGeneric::getDataObjectsFromArray($args,"user");
+
+        self::hasGeneratedCodeFromPaymentOrCenterRelease($payment,$order_header);
+
+        if(!$owner){
+            $owner = Session::getCurrentUser(throw:true);
+        }
+
+        $new_codes = new DB\codesList(" WHERE 0 ",[]);
+        for($x=0;$x<$qty;$x++){
+            $code = self::createNewProductCode($product,$owner,$payment,$payment_details,$order_header,$order_detail);
+            self::checkCodeIntegrity($code);
+            $code->save();
+            $user_from = $order_header ? DataUser::get($order_header->user_id) : null;
+            DataCodeOwnershipLog::addLog(DB\code_ownership_logX::$ACTIONS["purchase"],$code,$user_from,$owner,$code->time_purchased > 0 ? $code->time_purchased : $code->time_generated);
+            $new_codes->list[] = $code;
+        }
+        //endregion
+        return $new_codes;
+    }
+
+    public static function createNewProductCode(
+        DB\products $product,
+       ...$args,
+    ):DB\codesX{
+        /** @var DB\user $owner */
+        $owner = DataGeneric::getDataObjectsFromArray($args,"user");
+        /** @var DB\payment $payment */
+        $payment = DataGeneric::getDataObjectsFromArray($args,"payment");
+        /** @var DB\payment_details $payment_detail */
+        $payment_detail = DataGeneric::getDataObjectsFromArray($args,"payment_details");
+        /** @var DB\order_header $order_header */
+        $order_header = DataGeneric::getDataObjectsFromArray($args,"order_header");
+        /** @var DB\order_detail $order_detail */
+        $order_detail = DataGeneric::getDataObjectsFromArray($args,"order_detail");
+        $approved_by_user = null;
+
+        $new_code = new DB\codesX();
+        if($payment){
+            if(!$payment_detail) Assert::throw(error_message: "payment_detail is required",critical_error: true);
+            $owner = DataUser::get($payment->created_by);
+            $approved_by_user = DataUser::get($payment->approved_by);
+
+            $new_code->payment_ref = $payment->payment_referrence;
+            $new_code->time_purchased = $payment->time_approved;
+            $new_code->price_paid = $payment_detail->unit_price;
+            if($payment->ne_order > 0){
+                $new_code->ne_order_id = $payment->ne_order;
+            }
+        }
+        else if($order_header){
+            if(is_null($order_detail) && !($order_detail instanceof DB\order_detail)){
+                Assert::throw(error_message:"order_detail is required",critical_error: true);
+            }
+            $approved_by_user = DataUser::get($order_header->user_id);
+            $owner = $approved_by_user;
+            if($order_header->order_for > 0){
+                $owner = DataUser::get($order_header->order_for);
+            }
+            $new_code->order_id = $order_header->id;
+            $new_code->order_detail = $order_detail->id;
+        }
+        else if($owner){
+            $approved_by_user = $owner;
+        }
+        else{
+            $owner = Session::getCurrentUser(true);
+            $approved_by_user = $owner;
+        }
+
+        $new_code->code_type = "p";
+        $new_code->product_id = $product->id;
+        $new_code->product_tag = $product->product_tag;
+        $new_code->code = self::generateProductCode($product);
+        $new_code->pin = Random::getRandomInt(1000,9999);
+        $new_code->time_generated = TimeHelper::getCurrentTime()->getTimestamp();
+        $new_code->status = "o";
+        $new_code->purchased_by = $owner->id;
+        $new_code->owned_by = $owner->id;
+        $new_code->approved_by = $approved_by_user->id;
+        $new_code->save();
+        return $new_code;
+    }
+    #endregion END PRODUCTS
+
+
+    #region CHECKS
+
+    private static function hasGeneratedCodeFromPaymentOrCenterRelease(
+        int|string|DB\payment|false $payment,
+        int|string|DB\order_header|false $order_header,
+    ): void
+    {
+        if($payment){
+            $payment = DataPayment::get($payment);
+            if($payment->ne_order > 0){
+                $checkCodes = new DB\codesList(
+                    " WHERE ne_order_id=:order_id  "
+                    ,[":order_id"=>$payment->ne_order]);
+                if($checkCodes->count() > 0){
+                    Assert::throw(error_message:"Code already generated for ne_order",critical_error: true);
+                }
+            }
+            else{
+                $checkCodes = new DB\codesList(
+                    " WHERE payment_ref=:ref ",
+                    [":ref"=>$payment->payment_referrence]
+                );
+                if($checkCodes->count() > 0){
+                    Assert::throw(error_message: "Code already generated for payment_ref",critical_error: true);
+                }
+            }
+        }
+        if($order_header){
+            $order_header = DataOrderHeader::get($order_header);
+            $checkCodes = new DB\codesList(
+                " WHERE order_id=:order_id "
+                ,[":order_id"=>$order_header->id]);
+            if($checkCodes->count() > 0){
+                Assert::throw("code already generated for this order_header");
+            }
+        }
+    }
+
+    private static function checkCodeIntegrity(DB\codes $code): void
+    {
+        if(empty($code->product_id) && empty($code->variant_id))
+            Assert::throw(error_message:"no product_id and variant_id",critical_error: true);
+        if($code->variant_id > 0){
+            if(empty($code->package_id)) Assert::throw(error_message:"no package_id",critical_error: true);
+            if(empty($code->variant_tag)) Assert::throw(error_message: "no variant_tag",critical_error: true);
+            if($code->code_type != "e") Assert::throw(error_message: "expected code_type e",critical_error: true);
+        }
+        if($code->product_id > 0){
+            if(empty($code->product_tag)) Assert::throw(error_message:"product_tag missing",critical_error: true);
+        }
+    }
+
+    #endregion CHECKS
+
+
+    #region UTILITIES
+    public static function generateAccountNumber(): string{
+        do{
+            $accountNumber = Random::getRandomStr(length:9,options:Random::OPTION_ALPHA_SAFE_ALL);
+            $code = self::get(codes:$accountNumber,throw:false);
+        }while($code);
+        return $accountNumber;
+    }
+    public static function generateProductCode(DB\products $product): string{
+        Assert::recordExist($product);
+        do{
+            $left = "P" . str_pad($product->id, 2, '0', STR_PAD_LEFT);
+            $productNumber = $left . "-" . Random::getRandomStr(length:9,options: Random::OPTION_ALPHA_SAFE_HIGH);
+            $code = DataCodes::get(codes:$productNumber,baseOnly:true,throw:false);
+        }while($code);
+        return $productNumber;
+    }
+    static function addLog(string $log, int $line)
+    {
+        Logger::add(msg:$log,category: "codes",line:$line);
+    }
+    #endregion END UTILITIES
 }
