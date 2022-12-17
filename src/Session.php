69dd4f7b911cc23dc0c39cd9606011895ad7ca08
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-17 23:23:35 +0800

    refactor and added new helper methods

--- src/Session.php
+++ src/Session.php
@@ -8,10 +8,23 @@ class Session
 {
     public static ?DB\userX $current_user = null;
 
-    public static function getCurrentUser(): DB\userX|false{
-        if(isset($GLOBALS["user"])){
+    public static function sync(){
+        if(isset($GLOBALS["user"]) && is_null(self::$current_user)){
             self::$current_user = $GLOBALS["user"];
         }
+        else if(!is_null(self::$current_user)){
+            $GLOBALS["user"] = self::$current_user;
+        }
+    }
+
+    public static function getCurrentUser(bool $throw = true): DB\userX|false{
+        self::sync();
+        if($throw) self::assertIsLoggedIn();
         return self::$current_user ?? false;
+    }
+
+    public static function assertIsLoggedIn(){
+        self::sync();
+        if(is_null(self::$current_user)) Assert::throw("user is not logged in");
     }
 }
