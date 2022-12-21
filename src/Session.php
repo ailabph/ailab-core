4a54462ecf632006454a8b9730c85747e9fe62b0
adminailab <96946812+adminailab@users.noreply.github.com>
2022-12-22 00:56:04 +0800

    fix bug

--- src/Session.php
+++ src/Session.php
@@ -9,7 +9,7 @@ class Session
     public static ?DB\userX $current_user = null;
 
     public static function sync(){
-        if(isset($GLOBALS["user"]) && is_null(self::$current_user)){
+        if(isset($GLOBALS["user"]) && $GLOBALS["user"] instanceof DB\user && is_null(self::$current_user)){
             self::$current_user = $GLOBALS["user"];
         }
         else if(!is_null(self::$current_user)){
