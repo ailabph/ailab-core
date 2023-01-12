3438f998ffd7d7a8eae68b2a4a62100e05306f2b
adminailab <96946812+adminailab@users.noreply.github.com>
2023-01-12 18:14:54 +0800

    fix bug

--- src/DataAccount.php
+++ src/DataAccount.php
@@ -125,14 +125,12 @@ class DataAccount
         if(!empty($target_account->placement_account_id)){
             $placement = DataAccount::get($target_account->placement_account_id);
             if(empty($placement->dna)) Assert::throw("placement upline:$placement->account_code has an empty dna");
-            if(!empty($target_account->dna)) Assert::throw("placement upline information already set");
             $target_account->dna = $placement->dna."_".$target_account->position.$target_account->id;
         }
         $target_account->sponsor_dna = $target_account->id;
         if(!empty($target_account->sponsor_account_id)){
             $sponsor = DataAccount::get($target_account->sponsor_account_id);
             if(empty($sponsor->sponsor_dna)) Assert::throw("sponsor upline:$sponsor->account_code has an empty sponsor dna");
-            if(!empty($target_account->sponsor_dna)) Assert::throw("sponsor dna information already set:$target_account->sponsor_dna");
             $target_account->sponsor_dna = $sponsor->sponsor_dna."_".$target_account->id;
         }
         $target_account->save();
