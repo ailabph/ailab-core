fb98129a6417ddb7d36a3ae037addd5bbea1f35f
adminailab <96946812+adminailab@users.noreply.github.com>
2023-01-12 13:39:22 +0800

    test for cd, fs entry

--- tests/PaymentTest.php
+++ tests/PaymentTest.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 use Ailabph\AilabCore;
 use App\DBClassGenerator\DB;
+require_once("TestUtilities.php");
 
 class PaymentTest extends TestCase
 {
@@ -15,12 +16,15 @@ class PaymentTest extends TestCase
     protected function setUp(): void
     {
         AilabCore\Connection::startTransaction();
+        $this->testUtility = new TestUtilities();
     }
 
     protected function tearDown(): void
     {
         AilabCore\Connection::rollback();
     }
+
+    protected TestUtilities $testUtility;
 
     public function createPayment(): DB\paymentX{
         $admin = AilabCore\DataUser::create([]);
@@ -41,5 +45,60 @@ class PaymentTest extends TestCase
     public function testCreatePayment(){
         $payment = $this->createPayment();
         self::assertNotEmpty($payment->payment_referrence);
+    }
+
+    public function createEntryPayment():DB\paymentX{
+        AilabCore\DataTopUpCredits::$OVERRIDE_TOP_UP_ENABLE = false;
+        $payment = $this->createPayment();
+        $variant = $this->testUtility->getFirstPackageVariant();
+        $payment = AilabCore\DataPayment::createEntryPayment(
+            $payment,
+            [(object)[
+                "package_id" => $variant->package_id,
+                "variant_id" => $variant->id,
+                "qty" => 1
+            ]]);
+        $admin = $this->testUtility->createAdmin();
+        AilabCore\Session::$current_user = $admin;
+        return $payment;
+    }
+    public function testApproveEntryPayment(){
+        $payment = $this->createEntryPayment();
+        $payment->payment_mode = "cash";
+        $payment->save();
+        AilabCore\DataPayment::approvePayment(payment:$payment);
+
+        # check paid codes
+        $paid_codes = new DB\codesList(" WHERE payment_ref=:ref ",[":ref"=>$payment->payment_referrence]);
+        self::assertGreaterThan(0,$paid_codes->count(),"generated paid codes > 0");
+        while($code = $paid_codes->fetch()){
+            self::assertEmpty($code->special_type,"paid code special type must be empty or null");
+        }
+    }
+    public function testApproveEntryCdPayment(){
+        $payment = $this->createEntryPayment();
+        $payment->payment_mode = "cd";
+        $payment->save();
+        AilabCore\DataPayment::approvePayment($payment);
+
+        # check cd codes
+        $cd_codes = new DB\codesList(" WHERE payment_ref=:ref ",[":ref"=>$payment->payment_referrence]);
+        self::assertGreaterThan(0,$cd_codes->count(),"generated cd codes > 0");
+        while($code = $cd_codes->fetch()){
+            self::assertEquals("cd",$code->special_type,"cd code special_type");
+        }
+    }
+    public function testApproveEntryFsPayment(){
+        $payment = $this->createEntryPayment();
+        $payment->payment_mode = "fs";
+        $payment->save();
+        AilabCore\DataPayment::approvePayment($payment);
+
+        # check cd codes
+        $cd_codes = new DB\codesList(" WHERE payment_ref=:ref ",[":ref"=>$payment->payment_referrence]);
+        self::assertGreaterThan(0,$cd_codes->count(),"generated fs codes > 0");
+        while($code = $cd_codes->fetch()){
+            self::assertEquals("fs",$code->special_type,"fs code special_type");
+        }
     }
 }
